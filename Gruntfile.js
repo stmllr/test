e98bb3a63e2ff4c05f293bdc85bfba9599f3bc8b
Steffen Müller <typo3@t3node.com>
2014-11-26 11:00:57 +0100

    More build stuff by defaulr

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,7 +62,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-sass');
 
 	grunt.registerTask('dev', ['jshint', 'sass:dev']);
-	grunt.registerTask('acceptance', ['jshint', 'concat', 'sass:deploy', 'uglify']);
-	grunt.registerTask('production', ['jshint', 'concat', 'sass:deploy', 'uglify']);
+	grunt.registerTask('build-acceptance', ['jshint', 'concat', 'sass:deploy', 'uglify']);
+	grunt.registerTask('build-production', ['jshint', 'concat', 'sass:deploy', 'uglify']);
 	grunt.registerTask('default', ['jshint']);
 };
