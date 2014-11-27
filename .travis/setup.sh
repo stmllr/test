603331ef3b17f2207c9a76d9c33901f30e2764f0
Steffen Müller <typo3@t3node.com>
2014-11-27 13:10:47 +0100

    set command setup

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-BUILD_CONTEXT="dev" 
-
-
 echo -e "\n========================================"
 echo -e "\n Working on branch: ${TRAVIS_BRANCH}"
-echo -e "\n Pull request env: ${TRAVIS_PULL_REQUEST}"
-if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
-	echo -e "\n ... triggered by a pull request"
+
+
+# Make a test build, if this is a pull request or a different branch than master
+if [[ -n "${TRAVIS_PULL_REQUEST}" && "${TRAVIS_PULL_REQUEST}" != "false" && ${TRAVIS_BRANCH} == "master" ]]; then
+	GRUNT_COMMAND="build-test"
+	echo -e "\n Creating a test build for pull request ${TRAVIS_REPO_SLUG}#${TRAVIS_PULL_REQUEST}"
+elif [[ ${TRAVIS_BRANCH} == "master" ]]; then
+	GRUNT_COMMAND="build-production"
+	echo -e "\n Creating a production build."
+else
+	echo -e "\n Creating a dev build."
+	GRUNT_COMMAND="dev"
 fi
+
 echo -e "\n========================================"
 
-
-# If this is fork - just exit
-#if [[ -n "${TRAVIS_PULL_REQUEST}" && "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
-#fi
-
-if [ ${TRAVIS_BRANCH} == "master" ]; then
-	BUILD_COMMAND="grunt production"
-else
-	BUILD_COMMAND="grunt dev"
-fi
-
-export BUILD_COMMAND
-
+export GRUNT_COMMAND
