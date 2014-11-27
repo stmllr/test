ce5e59f589897beef039861914a2b44d35f00af3
Steffen Müller <typo3@t3node.com>
2014-11-27 12:46:06 +0100

    test script

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -2,22 +2,25 @@
 
 BUILD_CONTEXT="dev" 
 
+
 echo -e "\n========================================"
 echo -e "\n Working on branch: ${TRAVIS_BRANCH}"
-echo -e "\n Pull request: ${TRAVIS_PULL_REQUEST}"
+echo -e "\n Pull request env: ${TRAVIS_PULL_REQUEST}"
+if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
+	echo -e "\n ... triggered by a pull request"
+fi
 echo -e "\n========================================"
 
 
 # If this is fork - just exit
 #if [[ -n "${TRAVIS_PULL_REQUEST}" && "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
-#	echo -e '\n============== deploy will not be started (from the fork) ==============\n'
-#	exit 0
 #fi
 
-#if [ ${TRAVIS_BRANCH} == "master" ]; then
-#	BUILD_CONTEXT="production"
-#else
-#	BUILD_CONTEXT="dev"
-#fi
-#
-#export BUILD_CONTEXT
+if [ ${TRAVIS_BRANCH} == "master" ]; then
+	BUILD_COMMAND="grunt production"
+else
+	BUILD_COMMAND="grunt dev"
+fi
+
+export BUILD_COMMAND
+
